9a07f2cff6268fb2e34d385fbd3d720f50494448
hannah <hannah@hannah-mac.local>
2018-09-23 10:44:24 +0100

    simpler version

--- src/main.rs
+++ src/main.rs
@@ -16,30 +16,18 @@ mod sudoku {
     pub struct Partial {
         solution: Vec<Option<char>>,
     }
-    #[derive(Debug)]
-    pub enum Node {
-        Partial {
-            value: char,
-            solution: Partial,
-            children: Vec<Node>,
-        },
-        Done {
-            value: char,
-            solution: Vec<char>,
-        },
-        Root {
-            children: Vec<Node>,
-        },
-    }
 
     mod utils {
-        pub fn simplify(xs: &[Option<char>]) -> String {
+        /*pub fn simplify(xs: &[Option<char>]) -> String {
             xs.iter()
                 .map(|x| match x {
                     Some(v) => *v,
                     None => '-',
-                }).fold(String::new(),|mut acc,x|{acc.push(x);acc})
-        }
+                }).fold(String::new(), |mut acc, x| {
+                    acc.push(x);
+                    acc
+                })
+        }*/
         pub fn splice<T>(xs: &[Option<T>], y: T) -> Vec<Option<T>>
         where
             T: Clone,
@@ -51,6 +39,14 @@ mod sudoku {
                     Some(value) => Some((*value).clone()),
                     None => y_iter.next(),
                 }).collect::<Vec<Option<T>>>()
+        }
+        pub fn append<T>(xs: &[T], y: &T) -> Vec<T>
+        where
+            T: Clone,
+        {
+            use std::iter;
+            let y_iter = iter::once(y);
+            xs.into_iter().chain(y_iter).cloned().collect::<Vec<T>>()
         }
     }
 
@@ -140,19 +136,20 @@ mod sudoku {
             value: char,
             problem: &sudoku::Problem,
             parent_solution: &sudoku::Partial,
-        ) -> Option<sudoku::Node> {
+            solutions: &[Vec<char>],
+        ) -> Vec<Vec<char>> {
             use sudoku::rule;
             use sudoku::utils;
-            println!("----------create_child----------");
+            /*println!("----------create_child----------");
             println!("value:{:?}", &value);
             println!(
                 "parent_solution:{:?}",
                 utils::simplify(&parent_solution.solution)
-            );
+            );*/
             let trial_values = utils::splice(&parent_solution.solution, value);
-            println!("trial_values   :{:?}", utils::simplify(&trial_values));
+            //println!("trial_values   :{:?}", utils::simplify(&trial_values));
             let broken_rule_count = rule::check_rules(&trial_values, &problem.grid);
-            println!("broken_rule_count:{:?}", &broken_rule_count);
+            //println!("broken_rule_count:{:?}", &broken_rule_count);
             if broken_rule_count == 0 {
                 //see if we are done
                 let done = trial_values
@@ -160,220 +157,55 @@ mod sudoku {
                     .filter(|x| x.is_none())
                     .collect::<Vec<_>>()
                     .is_empty();
-                println!("done:{:?}", &done);
+                //println!("done:{:?}", &done);
                 if done {
                     let solution = trial_values
                         .iter()
                         .filter_map(|x| *x)
                         .collect::<Vec<char>>();
-                    println!("solution : {:?}", solution);
-                    println!("Done");
-                    println!("==========create_child==========");
-                    Some(sudoku::Node::Done {
-                        value: value,
-                        solution: solution,
-                    })
+                    //println!("solution : {:?}", solution);
+                    //println!("Done");
+                    //println!("==========create_child==========");
+
+                    utils::append(&solutions, &solution)
                 } else {
                     let partial_solution = sudoku::Partial {
                         solution: trial_values,
                     };
-                    let children = create_children(&problem, &partial_solution);
-
+                    let children = create_children(&problem, &partial_solution, &solutions);
                     //println!("partial_solution : {:?}", partial_solution);
-                    println!("number of children : {:?}", children.len());
-                    println!("Partial");
-                    println!("==========create_child==========");
-                    if children.is_empty() {
-                        None
-                    } else {
-                        Some(sudoku::Node::Partial {
-                            value: value,
-                            solution: partial_solution,
-                            children: children,
-                        })
-                    }
+                    //println!("number of children : {:?}", children.len());
+                    //println!("Partial");
+                    //println!("==========create_child==========");
+                    children.to_vec()
                 }
             } else {
-                None
+                solutions.to_vec()
             }
         }
         pub fn create_children(
             problem: &sudoku::Problem,
             parent_solution: &sudoku::Partial,
-        ) -> Vec<sudoku::Node> {
+            solutions: &[Vec<char>],
+        ) -> Vec<Vec<char>> {
             problem
                 .grid
                 .values
                 .iter()
-                .filter_map(move |&value| create_child(value, &problem, &parent_solution))
-                .collect::<Vec<sudoku::Node>>()
+                .flat_map(move |&value| create_child(value, &problem, &parent_solution, &solutions))
+                .collect::<Vec<Vec<char>>>()
         }
     }
-
-    struct IdGenerator {
-        curr: u32,
-    }
-
-    impl Iterator for IdGenerator {
-        type Item = u32;
-        fn next(&mut self) -> Option<u32> {
-            self.curr = self.curr + 1;
-            Some(self.curr)
-        }
-    }
-    impl IdGenerator {
-        fn new() -> IdGenerator {
-            IdGenerator { curr: 0 }
-        }
-    }
-    struct Dot {
-        generator: IdGenerator,
-        ids_and_labels: Vec<(u32, String)>,
-        edges: Vec<(u32, u32)>,
-    }
-
-    impl Dot {
-        fn new() -> Dot {
-            Dot {
-                generator: IdGenerator::new(),
-                ids_and_labels: Vec::new(),
-                edges: Vec::new(),
-            }
-        }
-        fn add_node(&mut self, label: &str) -> u32 {
-            let id = self.generator.next().unwrap();
-            self.ids_and_labels.push((id, label.to_owned()));
-            id
-        }
-        fn add_edge(&mut self, source: u32, target: u32) {
-            self.edges.push((source, target));
-        }
-        fn show(&self, name: &str) -> String {
-            fn show_id_and_label((id, label): &(u32, String)) -> String {
-                let mut out = id.to_string();
-                out += " [label=\"";
-                out += label.as_str();
-                out += "\"];\n";
-                out
-            }
-            fn show_edge((s, t): &(u32, u32)) -> String {
-                let mut out = s.to_string();
-                out += " -> ";
-                out += t.to_string().as_str();
-                out += " ;\n";
-                out
-            }
-
-            let mut out = "digraph ".to_owned();
-            out += name;
-            out += " {\n";
-            out += "node[shape=record];\n";
-            out += self
-                .ids_and_labels
-                .iter()
-                .map(show_id_and_label)
-                .fold(String::new(), |mut acc, x| {
-                    acc.push_str(x.as_str());
-                    acc
-                }).as_str();
-            out += self
-                .edges
-                .iter()
-                .map(show_edge)
-                .fold(String::new(), |mut acc, x| {
-                    acc.push_str(x.as_str());
-                    acc
-                }).as_str();
-            out += " }\n";
-            out
-        }
-    }
-
-    impl Node {
-        pub fn new(problem: &Problem) -> Node {
-            use sudoku;
-            use sudoku::helper;
-            Node::Root {
-                children: helper::create_children(
-                    &problem,
-                    &sudoku::Partial {
-                        solution: problem.values.clone(),
-                    },
-                ),
-            }
-        }
-        fn dot_label(&self) -> String {
-            use sudoku;
-            match self {
-                sudoku::Node::Root { children: _ } => "{root}".to_owned(),
-                sudoku::Node::Done { value, solution } => {
-                    let mut out = "{done|".to_owned();
-                    out.push(*value);
-                    out.push('|');
-                    out.push_str("[ ");
-                    out.push_str(
-                        solution
-                            .iter()
-                            .fold(String::new(), |mut acc, &x| {
-                                if acc.is_empty() {
-                                    acc.push(x);
-                                    acc
-                                } else {
-                                    acc.push_str(" , ");
-                                    acc.push(x);
-                                    acc
-                                }
-                            }).as_str(),
-                    );
-                    out.push_str(" ]");
-                    out += "}";
-                    out
-                }
-                sudoku::Node::Partial {
-                    value,
-                    solution: _,
-                    children: _,
-                } => {
-                    let mut out = String::new();
-                    out.push(*value);
-                    out
-                }
-            }
-        }
-
-        fn visit(&self, d: &mut Dot) -> u32 {
-            use sudoku;
-            let id = d.add_node(self.dot_label().as_str());
-            match self {
-                sudoku::Node::Done {
-                    value: _,
-                    solution: _,
-                } => (),
-                sudoku::Node::Partial {
-                    value: _,
-                    solution: _,
-                    children,
-                } => {
-                    children.iter().for_each(|child| {
-                        let child_id = child.visit(d);
-                        d.add_edge(id, child_id)
-                    });
-                }
-                sudoku::Node::Root { children } => {
-                    children.iter().for_each(|child| {
-                        let child_id = child.visit(d);
-                        d.add_edge(id, child_id)
-                    });
-                }
-            };
-            id
-        }
-
-        pub fn to_dot(self) -> String {
-            let mut dot = Dot::new();
-            self.visit(&mut dot);
-            dot.show("sudoku")
-        }
+    pub fn solve(problem: &Problem) -> Vec<Vec<char>> {
+        use sudoku;
+        use sudoku::helper;
+        helper::create_children(
+            &problem,
+            &sudoku::Partial {
+                solution: problem.values.clone(),
+            },
+            &[],
+        )
     }
 }
 
@@ -468,10 +300,10 @@ fn main() {
             None,
             None,
         ];
-        let solution = sudoku::Node::new(&sudoku::Problem {
+        let solution = sudoku::solve(&sudoku::Problem {
             grid: g,
             values: values,
         });
-        println!("{}", solution.to_dot());
+        println!("{:?}", solution);
     }
 }
