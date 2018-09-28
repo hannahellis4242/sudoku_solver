3910f8d7c4dd35266a8b700a42c952acf0dba0ba
hannah <hannah@hannah-mac.local>
2018-09-28 15:43:40 +0100

    working with json file input from command line

--- src/main.rs
+++ src/main.rs
@@ -3,108 +3,8 @@ extern crate itertools;
 extern crate serde_json;
 mod sudoku;
 use clap::{App, Arg};
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-
-fn foo() {
-    {
-        let g = sudoku::GridInfo {
-            height: 9,
-            width: 9,
-            square: 3,
-            values: vec!['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-        };
-        let values = vec![
-            None,
-            None,
-            Some('2'),
-            None,
-            None,
-            Some('6'),
-            None,
-            None,
-            Some('7'),
-            Some('5'),
-            Some('7'),
-            Some('9'),
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-            Some('4'),
-            None,
-            Some('2'),
-            Some('7'),
-            None,
-            Some('3'),
-            None,
-            None,
-            Some('4'),
-            None,
-            None,
-            Some('3'),
-            Some('8'),
-            Some('2'),
-            None,
-            None,
-            Some('1'),
-            Some('8'),
-            Some('5'),
-            None,
-            Some('9'),
-            None,
-            Some('4'),
-            None,
-            Some('3'),
-            Some('2'),
-            Some('2'),
-            None,
-            None,
-            Some('6'),
-            Some('5'),
-            Some('7'),
-            None,
-            None,
-            Some('4'),
-            None,
-            None,
-            Some('8'),
-            None,
-            Some('6'),
-            Some('5'),
-            None,
-            Some('4'),
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-            Some('7'),
-            Some('8'),
-            Some('9'),
-            Some('7'),
-            None,
-            None,
-            Some('1'),
-            None,
-            None,
-            Some('2'),
-            None,
-            None,
-        ];
-        let solution = sudoku::solve(&sudoku::Problem {
-            grid: g,
-            values: values,
-        });
-        println!("{:?}", solution);
-    }
-}
 
 trait ReadToString {
     fn read_to_str(&mut self) -> Result<String, std::io::Error>;
@@ -175,10 +75,7 @@ fn main() {
         ).get_matches();
     matches
         .value_of("INPUT")
-        .map(|x| {
-            println!("Using input file: {}", x);
-            x
-        }).and_then(read_file)
+        .and_then(read_file)
         .and_then(|x| match serde_json::from_str(&x) {
             Ok(v) => Some(v),
             Err(e) => {
@@ -186,6 +83,7 @@ fn main() {
                 None
             }
         }).map(|x: serde_json::Value| {
-            println!("{:?}", x);
-        });
+            json_to_sudoku(&x)
+        }).map(|problem| sudoku::solve(&problem))
+        .map(move |solutions| println!("{:?}",solutions));
 }
